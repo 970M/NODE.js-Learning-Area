aeb9a1e6e9811a3903aa14916d1eb31824559a70
970M <970M@protonmail.com>
2022-03-06 19:58:34 -0400

    Init.

---
+++ Getting-started/read-env-variable.js
@@ -0,0 +1,13 @@
+// Run app like this : USER_ID=239482 USER_KEY=foobar node read-env-variable.js
+
+let userId = process.env.USER_ID; // "239482"
+let userKey = process.env.USER_KEY; // "foobar"
+
+console.log("Hello world !!!");
+console.log(userId, userKey);
+
+require("dotenv").config();
+
+process.env.USER_ID1; // "239482"
+process.env.USER_KEY1; // "foobar"
+console.log(process.env.NODE_ENV1); // "development"
---
+++ README.md
@@ -0,0 +1,23 @@
+# NODE.js Learning Area
+
+> https://nodejs.dev/learn/
+
+## Quick start
+
+## Getting started
+
+> https://nodejs.dev/learn/the-v8-javascript-engine
+
+**Run :**
+
+    node app.js
+
+>
+
+    npm i -g nodemon // Module : Restart the application automatically
+    nodemon app.js>
+
+**Exit :**
+
+    ctrl-C
+    process.exit(1)
